24cc2c4699bc7a62e5f01e3870d614d9756f1412
Tree Roots <109141447+Tree-Roots@users.noreply.github.com>
2022-08-14 16:36:14 +0800

    3.1.5

---
+++ Packages/com.vrchat.avatars/Editor/VRCSDK/SDK3A/SDK3AImportFix.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using VRC.SDKBase.Editor.Source.Helpers;
+
+namespace VRC.SDK3A.Editor
+{
+    [InitializeOnLoad]
+    public class SDK3AImportFix
+    {
+        private const string avatarsReimportedKey = "AVATARS_REIMPORTED";
+        
+        private const string exampleScenePath =
+            "Packages/com.vrchat.avatars/Samples/Dynamics/Robot Avatar/Avatar Dynamics Robot Avatar.unity";
+
+        static SDK3AImportFix()
+        {
+            // Skip if we've already checked for the canary file during this Editor Session
+            if (!SessionState.GetBool(avatarsReimportedKey, false))
+            {
+                // Check for canary file in Library - package probably needs a reimport after a Library wipe
+                string canaryFilePath = Path.Combine("Library", avatarsReimportedKey);
+                if (File.Exists(canaryFilePath))
+                {
+                    SessionState.SetBool(avatarsReimportedKey, true);
+                }
+                else
+                {
+                    ReloadSDK();
+                    File.WriteAllText(canaryFilePath, avatarsReimportedKey);
+                }
+            }
+        }
+
+        [MenuItem("VRChat SDK/Samples/Avatar Dynamics Robot Avatar")]
+        private static void OpenAvatarsExampleScene()
+        {
+            EditorSceneManager.OpenScene(exampleScenePath);
+        }
+
+        public static async Task ReloadSDK()
+        {
+            // Set session key to true, limiting the reload to one run per session
+            SessionState.SetBool(avatarsReimportedKey, true);
+            
+            //Wait for project to finish compiling
+            while (EditorApplication.isCompiling || EditorApplication.isUpdating)
+            {
+                await Task.Delay(250);
+            }
+            
+            ReloadUtil.ReloadSDK();
+        }
+    }
+}
